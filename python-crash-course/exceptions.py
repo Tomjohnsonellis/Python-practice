3b6077478d0e2fcd1b3d6f0f2447c4bcd498855d
Thomas Johnson-Ellis <tomjohnsonellis@protonmail.com>
2023-02-03 10:21:07 +0000

    Add warnings to exceptions.py

--- python-crash-course/exceptions.py
+++ python-crash-course/exceptions.py
@@ -1,6 +1,12 @@
 """
 Some functions to raise exceptions
 """
+# Non-Fatal issues can be reported with these modules
+import warnings
+import logging
+def warn_user():
+    warnings.warn("You are not welcome here.")
+    logging.warning("Be careful!")
 
 
 # Making simple custom exceptions is easy thanks to OOP
@@ -16,9 +22,13 @@ def greet_user():
     name = input("Hello, what is your name? > ")
     if name.upper() == "BINGUS":
         raise BingusDetected
-    else:
-        print("Hello {}!".format(name))
+
+    if name.upper() == "BOB":
+        warn_user()
+
+    print("Hello {}!".format(name))
 
 
 if __name__ == "__main__":
     greet_user()
+    print("Have a wonderful day!")
